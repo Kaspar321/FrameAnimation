765b00080c1fd728258cd1c2dc84167d737a7b73
opilane <opilane@KLASSA402-25>
2018-04-27 09:11:47 +0300

    FrameAnimation

---
+++ FrameAnimation/FrameAnimation/MainActivity.cs
@@ -0,0 +1,29 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Graphics.Drawables;
+using Android.Views.Animations;
+
+namespace FrameAnimation
+{
+    [Activity(Label = "FrameAnimation", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Main);
+        }
+
+        public override void OnWindowFocusChanged(bool hasFocus)
+        {
+            if (hasFocus)
+            {
+                ImageView imageView = FindViewById<ImageView>(Resource.Id.animated_android);
+                AnimationDrawable animation = (AnimationDrawable)imageView.Drawable;
+                animation.Start();
+            }
+        }
+    }
+}
+
